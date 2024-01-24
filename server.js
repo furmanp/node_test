158fe8f79f8135ff4595ea252e859b20c0fcb00c
Przemek Furman <pfurman@twd.nl>
2024-01-24 08:29:32 +0100

    route added

--- server.js
+++ server.js
@@ -6,7 +6,10 @@ const app = express()
 
 app.use(express.json())
 app.get('/', (req, res)=>{
-    res.status(200).json("hey Imco")
+    res.status(200).json("test app")
+})
+app.get('/imco', (req, res)=>{
+    res.status(200).json("hey handsome")
 })
 app.listen(8080, () => {
     console.log("the app is running")
